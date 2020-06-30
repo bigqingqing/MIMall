ee1a4b99652ecab9679faf12751b17b0a5155810
bigqingqing <1842290287@qq.com>
2020-06-30 11:57:33 +0800

    项目初始化完毕

---
+++ src/views/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>index</h1>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>logins</h1>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style>
+
+</style>
